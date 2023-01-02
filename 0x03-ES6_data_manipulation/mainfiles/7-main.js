32d10704a72c5cc749f91536a150b028583578fc
Nimah <busayonimota1603@gmail.com>
2023-01-02 13:23:42 +0100

    [Feats}: Add test file for 7-has_array_values.js

---
+++ 0x03-ES6_data_manipulation/mainfiles/7-main.js
@@ -0,0 +1,8 @@
+import hasValuesFromArray from './7-has_array_values.js';
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 2, 3, 4]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [6, 1, 2, 3, 4]));
