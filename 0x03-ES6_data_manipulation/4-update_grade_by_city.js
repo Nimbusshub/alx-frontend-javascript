9de84f4b00c0dec943c881da12a87ecd7c0a01ad
Nimah <busayonimota1603@gmail.com>
2023-01-02 20:47:33 +0100

    [Fix]: Edge cases fixed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,13 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return students.filter((std) => std.location === city).map((std) => ({...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A',}));
+  return arr
+    .filter((std) => std.location === city)
+    .map((std) => ({
+      ...std,
+      grade:
+        newGrades
+          .filter((g) => g.studentId === std.id)
+          .map((g) => g.grade)[0] || 'N/A',
+    }));
 }
 
 // More explanatory code
