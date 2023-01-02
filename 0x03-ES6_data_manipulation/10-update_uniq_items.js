e73e8d89eff969d41af8f0d7c38ce0ce4ec0309e
Nimah <busayonimota1603@gmail.com>
2023-01-02 21:16:29 +0100

    [Fix]: Throw error when param is not a Map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,8 @@
 export default function updateUniqueItems(aMap) {
+  if (!(aMap instanceof Map)) throw new Error('Cannot process');
   for (const [key, value] of aMap.entries()) {
     if (value === 1) {
-      try {
-        aMap.set(key, 100);
-      } catch (err) {
-        return 'Cannot process';
-      }
+      aMap.set(key, 100);
     } else aMap.set(key, value);
   }
   return aMap;
