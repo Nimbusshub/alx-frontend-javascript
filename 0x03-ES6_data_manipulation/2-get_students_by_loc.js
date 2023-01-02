b57beefeb5a8c12d1ce199308f0571f1bd831435
Nimah <busayonimota1603@gmail.com>
2023-01-02 08:43:09 +0100

    [Feats]: return a filtered array

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(arr, city) {
   if (!Array.isArray(arr)) return [];
-  return arr.filter((val) => val...location === city);
+  return arr.filter((val) => val.location === city);
 }
