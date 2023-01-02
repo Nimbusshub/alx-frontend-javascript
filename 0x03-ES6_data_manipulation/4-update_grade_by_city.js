6ac248186d7efa97e221be1ac64685307dfa389f
Nimah <busayonimota1603@gmail.com>
2023-01-02 20:41:55 +0100

    [Fix]: Edge cases fixed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,28 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
-  if (!Array.isArray(arr)) return [];
-  const newArr = arr.filter((val) => val.location === city);
-
-  return newArr.map((val) => {
-    newGrades.forEach((ind) => {
-      if (ind.studentId === val.id) {
-        if (ind.grade) val.grade = `${ind.grade}`; // eslint-disable-line
-        else val.grade = 'N/A'; // eslint-disable-line
-      }
-    });
-    if (!val.grade) val.grade = 'N/A'; // eslint-disable-line
-    return val;
-  });
+  return students
+    .filter((std) => std.location === city)
+    .map((std) => ({
+      ...std,
+      grade:
+        newGrades
+          .filter((g) => g.studentId === std.id)
+          .map((g) => g.grade)[0] || 'N/A',
+    }));
 }
+
+// More explanatory code
+// export default function updateStudentGradeByCity(arr, city, newGrades) {
+//   if (!Array.isArray(arr)) return [];
+//   const newArr = arr.filter((val) => val.location === city);
+
+//   return newArr.map((val) => {
+//     newGrades.forEach((ind) => {
+//       if (ind.studentId === val.id) {
+//         if (ind.grade) val.grade = `${ind.grade}`; // eslint-disable-line
+//         else val.grade = 'N/A'; // eslint-disable-line
+//       }
+//     });
+//     if (!val.grade) val.grade = 'N/A'; // eslint-disable-line
+//     return val;
+//   });
+// }
