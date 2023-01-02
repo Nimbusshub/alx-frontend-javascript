cd8bc903d64dfedad71aba08bd655bfd218e981c
Nimah <busayonimota1603@gmail.com>
2023-01-02 14:44:31 +0100

    [Feats]: Return a map of groceries list

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const newmap = new Map();
+  newmap.set('Apples', 10);
+  newmap.set('Tomatoes', 10);
+  newmap.set('Pasta', 1);
+  newmap.set('Rice', 1);
+  newmap.set('Banana', 5);
+  return newmap;
+}
