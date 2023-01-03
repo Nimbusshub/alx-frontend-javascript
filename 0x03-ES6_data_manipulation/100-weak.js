1576912c02f3f1e9cf06837a9d8646bd19833fb1
Nimah <busayonimota1603@gmail.com>
2023-01-03 23:58:49 +0100

    [Feats]: Create a weakmap - weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,4 @@
 export const weakMap = new WeakMap();
-// let callTime = 1;
-
-// export default function queryAPI(endpoint) {
-//   weakMap.set(endpoint, callTime);
-//   callTime += 1;
-//   const callCount = weakMap.get(endpoint);
-//   if (callCount >= 5) {
-//     throw new Error('Endpoint load is high');
-//   }
-// }
-
 let callTime = 1;
 
 export function queryAPI(endPoint) {
