3da86fa508e94dbdc3a89cda10ca4f39f88263fc
Nimah <busayonimota1603@gmail.com>
2023-01-04 23:01:53 +0100

    [Feats]: Implement string lateral types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,3 +42,20 @@ export function createEmployee(salary: number | string): Teacher | Director {
   if (typeof salary === 'number' && salary < 500) return new Teacher();
   else return new Director();
 }
+
+export function isDirector(employee: Teacher | Director) {
+  return employee instanceof Director;
+}
+export function executeWork(employee: Teacher | Director) {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  } else return (employee as Teacher).workTeacherTasks();
+}
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else return 'Teaching History';
+}
