79994cb61953becd1fdc6d84fa85388c2ab38062
Nimah <busayonimota1603@gmail.com>
2023-01-04 21:50:11 +0100

    [Feats]: Create an interface for student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'Nimah',
+  lastName: 'Banjo',
+  age: 25,
+  location: 'Kwara',
+};
+const student2: Student = {
+  firstName: 'Jane',
+  lastName: 'Ohia',
+  age: 26,
+  location: 'Lagos',
+};
+
+const studentsList = [student1, student2];
